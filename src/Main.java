93f42c75a3e0ae267d341d1b51227c2e1378397d
Dante de Lordran <lordran@darsaras.dev>
2024-06-17 16:10:02 -0600

    Remove duplicates from sorted array

--- src/Main.java
+++ src/Main.java
@@ -7,20 +7,21 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums1 = {1,2,3,0,0,0};
-        int[] nums2 = {2,5,6};
+        //int[] nums1 = {1,2,3,0,0,0};
+        //int[] nums2 = {2,5,6};
         //System.out.println("Merge sorted array");
         //new MergeSortedArray().merge(nums1, 3, nums2, 3);
 
-        int[] nums3 = {0,0,1,1,1,2,2,3,3,4};
+        //int[] nums3 = {0,0,1,1,1,2,2,3,3,4};
         //System.out.println("Remove element");
         //new RemoveElement().removeElement(nums3, 2);
 
         //System.out.println("Remove duplicates from sorted array");
         //new RemoveDuplicatesFromSortedArray().removeDuplicates(nums3);
 
-        int[] nums4 = {0,0,1,1,1,1,2,3,3};
-        System.out.println("Remove duplicates 2");
-        new RemoveDuplicatesFromSortedArrayTwo().removeDuplicates(nums4);
+        int[] nums4 = {0,0,1,1,1,1,2,3,3,3,3,3,3,3,3,4,4,4,4,4,4,5};
+        int[] nums5 = {1,1,1,2,2,3};
+        //System.out.println("Remove duplicates 2");
+        new RemoveDuplicatesFromSortedArrayTwo().removeDuplicates(nums5);
     }
 }
--- src/leetcode/removeduplicatesfromsortedarraytwo/java/RemoveDuplicatesFromSortedArrayTwo.java
+++ src/leetcode/removeduplicatesfromsortedarraytwo/java/RemoveDuplicatesFromSortedArrayTwo.java
@@ -1,10 +1,15 @@
 package leetcode.removeduplicatesfromsortedarraytwo.java;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class RemoveDuplicatesFromSortedArrayTwo {
     public int removeDuplicates(int[] nums) {
-        Arrays.sort(nums);
-
+        int i=0;
+        for(int n:nums){
+            if(i<2 || n!=nums[i-2]){
+                nums[i++]=n;
+            }
+        }
+        return i;
     }
 }
